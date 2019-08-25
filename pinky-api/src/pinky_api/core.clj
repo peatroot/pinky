5f4365fd8b8d7c68b60f01de2e823c1ed8b646f1
peatroot <garethpeat@gmail.com>
2019-08-25 19:27:40 +0100

    Add lein generated api structure

---
+++ pinky-api/src/pinky_api/core.clj
@@ -0,0 +1,6 @@
+(ns pinky-api.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ pinky-api/test/pinky_api/core_test.clj
@@ -0,0 +1,7 @@
+(ns pinky-api.core-test
+  (:require [clojure.test :refer :all]
+            [pinky-api.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
