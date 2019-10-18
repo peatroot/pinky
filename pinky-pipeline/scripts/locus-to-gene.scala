459be6a9d0a6dc0e9e45c7d93efd42cae52770dd
peatroot <garethpeat@gmail.com>
2019-10-18 17:47:34 +0100

    Add variant and study entities

--- pinky-pipeline/scripts/locus-to-gene.scala
+++ pinky-pipeline/scripts/locus-to-gene.scala
@@ -1,9 +1,13 @@
 // creates a minimal json file from the preliminary locus-to-gene score
 val inFileL2g = sys.env("OT_GENETICS_ANALYSIS_L2G")
 val outFileG2v = sys.env("FILE_LOCUS_2_GENE")
+val outFileVariant = sys.env("FILE_ENTITY_VARIANT")
+val outFileStudy = sys.env("FILE_ENTITY_STUDY")
 
 // load the locus to gene data
 val l2g = spark.read.parquet("data/raw/" + inFileL2g)
+
+// --------------------------------------------------
 
 // extract just the g2v part (ignore studies)
 val g2v = l2g.select(
@@ -25,6 +29,36 @@ val g2v = l2g.select(
 
 // write out
 g2v.coalesce(1).write.mode("overwrite").json("data/raw/" + outFileG2v)
+
+// --------------------------------------------------
+
+// extract just the variants
+val variants = l2g.select(
+    concat(
+        l2g.col("chrom"),
+        lit("_"),
+        l2g.col("pos"),
+        lit("_"),
+        l2g.col("ref"),
+        lit("_"),
+        l2g.col("alt")
+    ).alias("variantId")
+).na.drop().dropDuplicates()
+
+// write out
+variants.coalesce(1).write.mode("overwrite").json("data/raw/" + outFileVariant)
+
+// --------------------------------------------------
+
+// extract just the studies
+val studies = l2g.select(
+    l2g.col("study_id").alias("studyId")
+).na.drop().dropDuplicates()
+
+// write out
+studies.coalesce(1).write.mode("overwrite").json("data/raw/" + outFileStudy)
+
+// --------------------------------------------------
 
 // exit (assuming usage with spark-shell -i)
 System.exit(0)
