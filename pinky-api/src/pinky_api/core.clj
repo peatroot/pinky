c3359635f5957660fe2a8f18cb3c3c758b02a1c4
peatroot <garethpeat@gmail.com>
2019-10-21 15:22:04 +0100

    Add get-interactors rule and query

--- pinky-api/src/pinky_api/core.clj
+++ pinky-api/src/pinky_api/core.clj
@@ -1,5 +1,6 @@
 (ns pinky-api.core
   (:require
+    [clara.rules.accumulators :as acc]
     [clara.rules :refer :all]
     [clojure.java.io :as io]
     [cheshire.core :as json])
@@ -32,6 +33,39 @@
 (defrecord MechanismOfAction [ensg-id chembl-id type mechanism-of-action])
 (defrecord ProteinProteinInteraction [ensg-id-1 ensg-id-2 score])
 
+; ; rules for inference
+; (defrecord TherapeuticHypothesisRemoteDrugInteraction [drug mechanism-of-action-type drug-interacting-gene gwas-locus-gene locus study trait])
+; (defrule infer-therapeutic-hypothesis--remote-drug-interaction
+;   "Matches cases where:
+;   * a drug Dr interacts with a gene G1 (via mechanism type T)
+;   * gene G1 interacts with another gene G2 (via a protein protein interaction)
+;   * gene G2 is associated with a GWAS study S (for trait E at locus L)"
+;   [MechanismOfAction (= ?g1 ensg-id) (= ?dr chembl-id) (= ?t type)]
+;   [ProteinProteinInteraction (= ?p1 ensg-id-1) (= ?p2 ensg-id-2)]
+;   [L2G (= ?g2 ensg-id) (= ?l variant-id) (= ?s study-id) (= ?e efo-id)]
+;   [:test (= ?g1 ?p1) (= ?g2 ?p2)]
+;   =>
+;   (insert! (->TherapeuticHypothesisRemoteDrugInteraction ?dr ?t ?g1 ?g2 ?l ?s ?e))
+;   (println (format "TherapeuticHypothesisRemoteDrugInteraction(%s, %s, %s, %s, %s, %s, %s)" ?dr ?t ?g1 ?g2 ?l ?s ?e)))
+
+(defrecord Interactors [ensg-id interactors])
+(defrule get-interactors
+  [?ensg-id <- Gene (= ?ensg-id ensg-id)]
+  ; [?interactors <- (acc/all :interactor) :from [ProteinProteinInteraction (= ?ensg-id ensg-id-1) (= ?interactor ensg-id-2)]]
+  [?interactors <- (acc/all :ensg-id-2) :from [ProteinProteinInteraction (= ?ensg-id ensg-id-1)]]
+  =>
+  (insert! (->Interactors ?ensg-id ?interactors)))
+
+
+; (defrule propagate-subtrait-association
+;     "An association with gene G of a subtrait S of trait T infers an association of gene G with trait T"
+;     [IsSubTrait (= ?subtrait efo-id-1) (= ?supertrait efo-id-2)]
+;     [TraitGeneAssociation (= ?trait efo-id) (= ?gene ensg-id)]
+;     [:test (= ?subtrait ?trait)]
+;     =>
+;     (insert! (->TraitGeneAssociation ?supertrait ?gene))
+;     (println (str ?supertrait " inferred to be associated with " ?gene)))
+
 (defquery get-gene-by-symbol
   "Query to find a gene given the symbol."
   [:?symbol]
@@ -52,10 +86,26 @@
   [:?ensg-id]
   [?moa <- MechanismOfAction (= ?ensg-id ensg-id)])
 
+; (defquery get-interactors-for-gene-by-ensg-id
+;   "Query to find interactors for a gene given the ensgId"
+;   [:?ensg-id]
+;   [?ppi <- ProteinProteinInteraction (= ?ensg-id ensg-id-1)])
+
 (defquery get-interactors-for-gene-by-ensg-id
   "Query to find interactors for a gene given the ensgId"
   [:?ensg-id]
-  [?ppi <- ProteinProteinInteraction (= ?ensg-id ensg-id-1)])
+  [?result <- Interactors (= ?ensg-id ensg-id)])
+
+; ; works
+; (defquery get-interactors-for-gene-by-ensg-id
+;   "Query to find interactors for a gene given the ensgId"
+;   [:?ensg-id]
+;   [?interactors <- (acc/all :ensg-id-2) :from [ProteinProteinInteraction (= ?ensg-id ensg-id-1)]])
+
+; (defrule get-interactors
+;   [?ensg-id <- Gene (= ?ensg-id ensg-id)]
+;   ; [?interactors <- (acc/all :interactor) :from [ProteinProteinInteraction (= ?ensg-id ensg-id-1) (= ?interactor ensg-id-2)]]
+;   [?interactors <- (acc/all :ensg-id-2) :from [ProteinProteinInteraction (= ?ensg-id ensg-id-1)]]
 
 (defn -main
   "Basic setup."
@@ -68,9 +118,17 @@
       (insert-all (map (fn [d] (->ProteinProteinInteraction (d :gene1) (d :gene2) (d :score))) protein-protein-interactions))
       (fire-rules))]
     
-    (println 
-      (query initial-session get-gene-by-symbol :?symbol "BRAF"))
-    (println 
-      (query initial-session get-drug-by-name :?name "BEPRIDIL"))
+    ; (println 
+    ;   (query initial-session get-gene-by-symbol :?symbol "BRAF"))
+    ; (println 
+    ;   (query initial-session get-drug-by-name :?name "BEPRIDIL"))
+
     (println
-      (query initial-session get-interactors-for-gene-by-ensg-id :?ensg-id "ENSG00000004059"))))
+      (query initial-session get-interactors-for-gene-by-ensg-id :?ensg-id "ENSG00000157764")) ; BRAF
+
+    ; (println
+    ;   (get (query initial-session get-interactors-for-gene-by-ensg-id :?ensg-id "ENSG00000157764")) :interactors) ; BRAF
+    ; (println
+    ;   (get (query initial-session get-interactors-for-gene-by-ensg-id :?ensg-id "ENSG00000171862")) :interactors) ; PTEN
+  
+      ))
